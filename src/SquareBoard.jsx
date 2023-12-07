10aa08508a282a9e023fdadd4dd1495d99a2e58a
Arzu Caner <arzuguneycaner@gmail.com>
2023-12-07 14:17:05 +0000

    Implement chess pieces interactivity
    Enable the interactive behaviour for chess

--- src/SquareBoard.jsx
+++ src/SquareBoard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
+import { DragPreviewImage, useDrag } from 'react-dnd';
 
 const SquareBoard = ({ brd }) => {
+  const [collected, drag, preview] = useDrag({
+    type: 'chess',
+    item: { id: `${brd.type}_${brd.color}` }
+  })
   const pieceImage = require(`../public/assets/images/${brd.type}_${brd.color}.png`)
   console.log("brd", brd);
   return (
-    <div>
-      <img src={pieceImage} alt="" />
+    <div ref={drag}>
+      <DragPreviewImage src={pieceImage} connect={preview} />
+      <img className='w-[40px]' src={pieceImage} alt="" />
     </div>
   )
 }
