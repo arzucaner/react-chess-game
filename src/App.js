2b97a24a9d878d5353daf35744fb9482b9c93b6c
Arzu Caner <arzuguneycaner@gmail.com>
2023-12-11 21:17:55 +0000

    Add end-game rules
    In this commit, added the necessary logic and conditions to determine when the game is completed successfully. The end-game rules include checking for specific conditions or achievements that signify the successful completion of the game. These rules are essential for providing a clear and satisfying conclusion to the gaming experience.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import Board from './Board';
-import subjectGame from './Game';
-import { useEffect, useState } from 'react'
+import subjectGame, { initGame } from './Game';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   const [board, setBoard] = useState([]);
+  const [isGameOver, setIsGameOver] = useState();
+  const [result, setResult] = useState();
+
   useEffect(() => {
     initGame()
-    const subscribe = subjectGame.subscribe(sub => setBoard(sub.chess))
+    const subscribe = subjectGame.subscribe(sub => {
+      setBoard(sub.chess)
+      setIsGameOver(sub.isGameOver)
+      setResult(sub.result)
+    })
 
     return () => subscribe.unsubscribe();
   }, [])
+
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className='bg-terracotta h-screen flex items-center justify-center'>
+
+      <div className='bg-terracotta h-screen flex items-center justify-center relative'>
+        {
+          isGameOver && <div className='absolute bg-white bg-opacity-80 rounded-lg p-3'>
+            <h1>GAME OVER!!!</h1>
+          </div>
+        }
         <Board board={board} />
       </div>
     </DndProvider>
--- src/Game.js
+++ src/Game.js
@@ -24,16 +24,29 @@ export const move = (from, to) => {
 
 const updateGame = () => {
     const isGameOver = chess.isGameOver();
-    subjectGame.next({ chess: chess.board(), isGameOver, })
+    subjectGame.next({ chess: chess.board(), isGameOver, result: isGameOver ? getGameResult() : null})
 }
 
 const getGameResult = () => {
     if (chess.isCheckmate()) {
         const winner = chess.turn() === 'w' ? 'Black' : 'White';
         return `CHECKMATE - Winner : ${winner}`
-
+    } else if (chess.isDraw()) {
+        let reason = "50 Move Rule";
+        if (chess.isStalemate) {
+            reason = 'infinite loop'
+        } else if (chess.isThreefoldRepetition()) {
+            reason = 'Repetition'
+        } else if (chess.isInsufficientMaterial()) {
+            reason = 'Insufficient Materials'
+        }
+        return reason
+    } else {
+        return 'Unknown Situation'
     }
 }
+
+
 
 
 
