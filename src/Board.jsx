2898f2658cce00d4708a5a2845cf62946a463272
Arzu Caner <arzuguneycaner@gmail.com>
2023-12-20 12:50:27 +0000

    Add game state management to board component
    Game state management has been added. A state has been introduced to the Board component, and it is updated with the click event on the Squares. A state object has been created to hold information such as the current player's turn and move count. The handleSquareClick function is used to update the game state when a square is clicked.

--- src/Board.jsx
+++ src/Board.jsx
@@ -7,31 +7,48 @@ import Square from './Square';
 const Board = ({ board }) => {
   //console.log("board", board.flat())
 
-  const colorCntrl = (i) => {
-    const x = i % 8;
-    const y = Math.abs(Math.floor(i / 8) - 7)
-    return (x + y) % 2 === 0
+  const Board = ({ board }) => {
+
+    const [gameState, setGameState] = useState({
+      currentPlayer: 'white',
+      moveCount: 0,
+      gameStatus: 'in-progress',
+    });
+
+    const colorCntrl = (i) => {
+      const x = i % 8;
+      const y = Math.abs(Math.floor(i / 8) - 7)
+      return (x + y) % 2 === 0
+    }
+
+    const positionCntrl = (i) => {
+      const x = i % 8;
+      const letters = ["a", "b", "c", "d", "e", "f", "g", "h"][x]
+      const y = Math.abs(Math.floor(i / 8) - 7)
+      return `${letters}${y + 1}`
+    }
+
+    const handleSquareClick = (position) => {
+
+      setGameState((prevState) => ({
+        ...prevState,
+        moveCount: prevState.moveCount + 1,
+        currentPlayer: prevState.currentPlayer === 'white' ? 'black' : 'white',
+      }));
+    };
+
+    return (
+      <div className='w-[640px] h-[640px] bg-red-700 flex flex-wrap'>
+        {
+          board.flat().map((brd, i) => (
+            <Square colorValue={colorCntrl(i)} positionCntrl={positionCntrl(i)}>
+              {brd && <SquareBoard brd={brd} positionCntrl={positionCntrl(i)} />}
+            </Square>
+          ))
+        }
+      </div >
+    )
   }
-
-  const positionCntrl = (i) => {
-    const x = i % 8;
-    const letters = ["a", "b", "c", "d", "e", "f", "g", "h"][x]
-    const y = Math.abs(Math.floor(i / 8) - 7)
-    return `${letters}${y + 1}`
-
-  }
-
-  return (
-    <div className='w-[640px] h-[640px] bg-red-700 flex flex-wrap'>
-      {
-        board.flat().map((brd, i) => (
-          <Square colorValue={colorCntrl(i)} positionCntrl={positionCntrl(i)}>
-            {brd && <SquareBoard brd={brd} positionCntrl={positionCntrl(i)} />}
-          </Square>
-        ))
-      }
-    </div >
-  )
 }
 
 export default Board
