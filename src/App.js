86ef6ad8b2bfa07b9e1c3c38a105932b2a9e09ae
Arzu Caner <arzuguneycaner@gmail.com>
2023-12-21 21:25:50 +0000

    Add in-game controls: Start and Reset buttons
    This commit introduces in-game controls. Start and Reset buttons are now available to initiate and reset the game. The changes include modifications to relevant components and game logic, making the code more modular and user-friendly.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Board from './Board';
 import subjectGame, { initGame } from './Game';
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import subjectGame from './Game';
-
-
 
 function App() {
   const [board, setBoard] = useState([]);
-  const [isGameOver, setIsGameOver] = useState();
-  const [result, setResult] = useState();
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleStartGame = () => {
+    setIsLoading(true);
+    initGame();
+  };
+
+  const handleResetGame = () => {
+    setIsLoading(true);
+    initGame();
+  };
 
   useEffect(() => {
     initGame()
@@ -19,22 +27,25 @@ function App() {
       setBoard(sub.chess)
       setIsGameOver(sub.isGameOver)
       setResult(sub.result)
-    })
+    });
 
     return () => subscribe.unsubscribe();
-  }, [])
+  }, []);
 
   return (
     <DndProvider backend={HTML5Backend}>
-      
+
       <div className='bg-black h-screen flex items-center justify-center relative'>
-        {
-          isGameOver && <div className='absolute bg-white bg-opacity-80 rounded-lg p-3 flex flex-col items-center justify-center'>
+        {isGameOver && (
+          <div className='absolute bg-white bg-opacity-80 rounded-lg p-3 flex flex-col items-center justify-center'>
             <h1 className='font-bold'>GAME OVER!CONGRATULATIONS🎉 </h1>
             {result && <div>{result}</div>}
           </div>
-        }
-        <Board board={board} />
+        )}
+        {!isLoading && <Board board={board} />}
+        {!isLoading && !isGameOver && (
+          <button onClick={handleStartGame}>Start New Game</button>
+        )}
       </div>
     </DndProvider>
   );
